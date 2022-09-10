0916d8d0511696d7df763cdbf77d37914939c7cf
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2022-09-10 11:40:07 -0300

    Creates LobbyApi and uses it ate Home

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import _uniqueId from 'lodash/uniqueId'
 // import { LobbyClient } from 'boardgame.io/client'
 // import { LobbyAPI } from 'boardgame.io'
 import { useNavigate } from 'react-router-dom'
+import { LobbyAPI } from 'boardgame.io'
 import Button from '../../components/Button'
 import ClickableItem from '../../components/ClickableItem'
 import Input from '../../components/Input'
@@ -11,69 +12,31 @@ import Title from '../../components/Title'
 import { Container, HorizontalDiv, MainUI } from './HomePage.styles'
 import AvatarPick from '../../components/AvatarPick'
 import Switch from '../../components/Switch'
+import LobbyApi from './LobbyApi'
 
 function HomePage() {
+  const [playerName, setPlayerName] = useState<string>('')
   const [selecao, setSelecao] = useState<string>('Jogo rápido')
-
+  const [matchList, setMatchList] = useState<LobbyAPI.Match[]>([])
   const navigate = useNavigate()
 
-  const itens = [
-    <ClickableItem
-      title='Flavinho do Pneu'
-      actionText='Entrar'
-      action={() => alert('clicou')}
-      key={_uniqueId()}
-    />,
-    <ClickableItem
-      title='Flavinho do Pneu'
-      actionText='Entrar'
-      action={() => alert('clicou')}
-      key={_uniqueId()}
-    />,
-    <ClickableItem
-      title='Flavinho do Pneu'
-      actionText='Entrar'
-      action={() => alert('clicou')}
-      key={_uniqueId()}
-    />,
-    <ClickableItem
-      title='Flavinho do Pneu'
-      actionText='Entrar'
-      action={() => alert('clicou')}
-      key={_uniqueId()}
-    />,
-    <ClickableItem
-      title='Flavinho do Pneu'
-      actionText='Entrar'
-      action={() => alert('clicou')}
-      key={_uniqueId()}
-    />,
-    <ClickableItem
-      title='Flavinho do Pneu'
-      actionText='Entrar'
-      action={() => alert('clicou')}
-      key={_uniqueId()}
-    />,
-    <ClickableItem
-      title='Flavinho do Pneu'
-      actionText='Entrar'
-      action={() => alert('clicou')}
-      key={_uniqueId()}
-    />,
-    <ClickableItem
-      title='Flavinho do Pneu'
-      actionText='Entrar'
-      action={() => alert('clicou')}
-      key={_uniqueId()}
-    />,
-    <ClickableItem
-      title='Flavinho do Pneu'
-      actionText='Entrar'
-      action={() => alert('clicou')}
-      key={_uniqueId()}
-    />,
-  ]
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await LobbyApi.listMatches()
+      setMatchList(data)
+    }
 
+    fetchData()
+  }, [])
+
+  const createMatch = async () => {
+    const matchID = LobbyApi.createMath(playerName)
+    console.log(`match ${matchID} created`)
+  }
+
+  const playerNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPlayerName(e.target.value)
+  }
   return (
     <Container>
       <Title>Velha</Title>
@@ -83,12 +46,19 @@ function HomePage() {
           setOption={setSelecao}
         />
         <HorizontalDiv>
-          <Input type='text' placeholder='Seu apelido' />
+          <Input type='text' placeholder='Seu apelido' value={playerName} onChange={playerNameChange} />
           <AvatarPick avatar='👩🏼' />
         </HorizontalDiv>
         {selecao === 'Ver oponentes' && (
-          <List action={{ text: 'Criar jogo', action: () => alert('ola') }}>
-            {itens}
+          <List action={{ text: 'Criar jogo', action: () => createMatch() }}>
+            {matchList.map((match) => (
+              <ClickableItem
+                title={match?.setupData?.matchName || 'Partida sem nome'}
+                actionText='Entrar'
+                action={() => alert(match?.setupData?.matchName || 'Partida sem nome')}
+                key={_uniqueId()}
+              />
+            ))}
           </List>
         )}
         {selecao === 'Jogo rápido' && (
---
+++ src/pages/Home/LobbyApi.ts
@@ -0,0 +1,59 @@
+import { LobbyClient } from 'boardgame.io/client'
+
+const SERVER_URL = process.env.SERVER_URL || 'http://localhost'
+const SERVER_PORT = process.env.PORT || '8000'
+const SERVER = `${SERVER_URL}:${SERVER_PORT}`
+const GAME_NAME = process.env.GAME_NAME || 'JogoDaVelha'
+
+const lobbyClient = new LobbyClient({ server: SERVER })
+
+export default {
+  listMatches: async () => {
+    const { matches } = await lobbyClient.listMatches(GAME_NAME)
+    return matches
+  },
+  getMatch: async (matchID: string) => {
+    const match = await lobbyClient.getMatch(GAME_NAME, matchID)
+    return match
+  },
+  createMath: async (matchName: string) => {
+    const { matchID } = await lobbyClient.createMatch(GAME_NAME, {
+      numPlayers: 2,
+      setupData: {
+        matchName,
+      },
+    })
+
+    return matchID
+  },
+  joinMatch: async (
+    matchID: string,
+    playerName: string,
+    playerIDJoin?: string,
+  ) => {
+    const { playerCredentials, playerID } = await lobbyClient.joinMatch(
+      GAME_NAME,
+      matchID,
+      {
+        playerID: playerIDJoin,
+        playerName,
+      },
+    )
+
+    return { playerCredentials, playerID }
+  },
+  leaveMatch: async (matchID: string, playerID: string, credentials: string) => {
+    await lobbyClient.leaveMatch(GAME_NAME, matchID, {
+      playerID,
+      credentials,
+    })
+  },
+  playAgain: async (matchID: string, playerID: string, credentials: string) => {
+    const { nextMatchID } = await lobbyClient.playAgain(GAME_NAME, matchID, {
+      playerID,
+      credentials,
+    })
+
+    return nextMatchID
+  },
+}
