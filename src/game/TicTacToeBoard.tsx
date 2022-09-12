21047c1b8e1400f07157f7f5e0bd2ad92b2e1b1f
juliano.barbosa <juliano.barbosa@lambda3.com.br>
2022-09-12 19:09:00 -0300

    adds draft of leave button function

--- src/game/TicTacToeBoard.tsx
+++ src/game/TicTacToeBoard.tsx
@@ -5,22 +5,42 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useState } from 'react'
 import { BoardProps } from 'boardgame.io/react'
+import { useNavigate } from 'react-router-dom'
 import { TicTacToeState } from './Game'
 import Board from '../components/Board'
+import Button from '../components/Button'
+import LobbyApi from '../api/LobbyApi'
+import { useQueryParams } from '../hooks/useQueryParams'
 
 interface TicTacToeProps extends BoardProps<TicTacToeState> {}
 
 export function TicTacToeBoard({
   ctx, G, moves, matchData,
 }: TicTacToeProps) {
+  const navigate = useNavigate()
+
+  const query = useQueryParams()
+  const playerId = query.get('playerID') || ''
+  const credentials = query.get('token') || ''
+  const matchId = query.get('match') || ''
+
+  const exitMatch = () => {
+    LobbyApi.leaveMatch(matchId, playerId, credentials)
+    navigate('/')
+  }
   return (
-    <Board
-      player={ctx.currentPlayer}
+    <>
+      <Button variation='cancel' onClick={exitMatch}>
+        Sair
+      </Button>
+      <Board
+        player={ctx.currentPlayer}
         // setPlayer={setPlayer}
         // setCells={setCells}
-      moveFunction={moves.clickCell}
-      cells={G.cells}
-      valueMapping={{ 0: 'X', 1: 'O' }}
-    />
+        moveFunction={moves.clickCell}
+        cells={G.cells}
+        valueMapping={{ 0: 'X', 1: 'O' }}
+      />
+    </>
   )
 }
